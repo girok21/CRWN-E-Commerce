46d83c453d024f1da1a60dfb50ab37a50d927566
girok21 <hardeepsinha21@gmail.com>
2022-11-02 12:08:04 +0530

    Brought in firebase libraries and updated firebase component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,12 @@ import {
     signInWithPopup,
     GoogleAuthProvider,
 } from 'firebase/auth';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAfKjkRiTKEisEECTaRCMeN1l5iJXXU7OY",
@@ -25,3 +31,27 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  console.log(userDocRef);
+  const userSnapshot = await getDoc(userDocRef);
+  if(!userSnapshot.exists()) {//if user data does not exists
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    
+    try{
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error){
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+}
